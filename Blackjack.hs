e241470cb53d25aaa1a4152c9eaaae1fe00eab44
JorgeCaldeira <caldeirajorge03@gmail.com>
2023-11-18 03:02:47 +0000

    ...

--- Blackjack.hs
+++ Blackjack.hs
@@ -3,12 +3,15 @@
 module Blackjack where
 import BaralhosExemplo
 import Data.List
+import Data.Char
+
+
+--passar para type Baralho?--
 data Baralho =  Baralho {cartas::[String]}
     deriving (Show, Eq, Ord)
-    
 
 converte :: [String] -> Baralho
-converte xs = Baralho xs
+converte xs = Baralho (id xs)
 
 tamanho :: Baralho -> Int
 tamanho (Baralho {cartas = deck}) = foldr(\x acc -> acc+1) 0 deck
@@ -38,8 +41,8 @@ terminado :: EstadoJogo -> Bool
 terminado t = (tamanho (baralho t) <= 20) || (creditos t <= 0)
 
 instance Show EstadoJogo where
-    show s = --"jogador: " ++ Prelude.show (cartasJogador s) ++ "\ncasa: " ++ Prelude.show (cartasCasa s) ++ "\ncreditos: " ++ Prelude.show(creds s)
-        unlines ["jogador: " ++  Prelude.show (cartasJogador s), "casa: " ++ Prelude.show (cartasCasa s) , "creditos: " ++ Prelude.show(creds s) ]
+    show s = "jogador: " ++ Prelude.show (cartasJogador s) ++ "\ncasa: " ++ Prelude.show (cartasCasa s) ++ "\ncreditos: " ++ Prelude.show(creds s)
+        --unlines ["jogador: " ++  Prelude.show (cartasJogador s), "casa: " ++ Prelude.show (cartasCasa s) , "creditos: " ++ Prelude.show(creds s) ]
         --intercalate "\n" ["jogador: " ++ Prelude.show (cartasJogador s), "casa: " ++ Prelude.show (cartasCasa s), "creditos: " ++ Prelude.show (creds s)]
 
 -- mudar argumentos? 
@@ -65,29 +68,37 @@ sempreHit = Estrategia {aposta = 5,
                           descricao = "apostar sempre 5 créditos, fazer sempre hit"}
 
 --perceber e alterar
-{--estrategia3 :: Estrategia
-estrategia3 = { aposta = 5,
+estrategia3 :: Estrategia
+estrategia3 = Estrategia{aposta = 5,
                jogada = Stand,
                descricao = "a definir"}
---}
+
 {-simula uma ronda do jogo Blackjack, utilizando a estratégia dada. A função
 devolve o estado de jogo no final da ronda (atualizando o valor dos créditos e
 do baralho).
 -}
-simulaRonda :: Estrategia -> EstadoJogo -> EstadoJogo
-simulaRonda strat state = if (not (terminado state))
-    then (simulaRonda Estrategia {creds = (creditos state)  - strat.aposta}) else ()
-
-
+--simulaRonda :: Estrategia -> EstadoJogo -> EstadoJogo
+{--simulaRonda strat state = EstadoJogo{(curDeck = x:y:z:w:(baralho state)),
+                                     creds = creditos state
+                                     cartasJogador = x:y:[],
+                                     cartasCasa = z:w:[],
+                                     terminado = False
+                                    }
+--}
 {-dada uma
 estratégia do jogador e um baralho, corre uma simulação de um jogo
 completo de Blackjack, com um saldo inicial de 100 créditos. A função
 devolve o número de créditos do jogador no final da simulação.-}
-simulaJogo :: Estrategia -> Baralho -> Int
-simulaJogo strat state = 
+--imulaJogo:: Estrategia -> Baralho -> Int
+--simulaJogo strat deck = 
 
-}
 
 instance Show Estrategia where 
     show s = "Estrategia: " ++ descricao s
 
+valoresMao :: [String]-> Int
+valoresMao d = foldl(\acc x -> b x acc) 0 d
+    where b x acc
+            | head x == 'A' = if (acc + 11 > 21) then (acc + 1) else (acc + 11)
+            | elem (head x) "23456789" =  acc + digitToInt (head x)
+            | otherwise = acc + 10
